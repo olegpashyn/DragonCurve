9ccd90613e15e48b518b0ea7146ac18085c26112
Oleh Pashyn <oleh.pashyn@tobii.com>
2020-12-18 12:52:17 +0200

    Resolved

--- DragonFractalTask.cs
+++ DragonFractalTask.cs
@@ -1,32 +1,34 @@
-﻿// В этом пространстве имен содержатся средства для работы с изображениями. 
-// Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
-using System.Drawing;
+﻿using System;
 
 namespace Fractals
 {
 	internal static class DragonFractalTask
 	{
 		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
-		{
-			/*
-			Начните с точки (1, 0)
-			Создайте генератор рандомных чисел с сидом seed
-			
-			На каждой итерации:
-
-			1. Выберите случайно одно из следующих преобразований и примените его к текущей точке:
-
-				Преобразование 1. (поворот на 45° и сжатие в sqrt(2) раз):
-				x' = (x · cos(45°) - y · sin(45°)) / sqrt(2)
-				y' = (x · sin(45°) + y · cos(45°)) / sqrt(2)
-
-				Преобразование 2. (поворот на 135°, сжатие в sqrt(2) раз, сдвиг по X на единицу):
-				x' = (x · cos(135°) - y · sin(135°)) / sqrt(2) + 1
-				y' = (x · sin(135°) + y · cos(135°)) / sqrt(2)
-		
-			2. Нарисуйте текущую точку методом pixels.SetPixel(x, y)
-
-			*/
-		}
+        {
+            var random = new Random(seed);
+            double x = 1;
+            double y = 0;
+            pixels.SetPixel(x, y);
+            for (var i = 1; i <= iterationsCount; i++)
+            {
+                var nextNumber = random.Next(2);
+                double x1;
+                double y1;
+                if (nextNumber == 0)
+                {
+                    x1 = (x * Math.Cos(0.785398) - y * Math.Sin(0.785398)) / Math.Sqrt(2);
+                    y1 = (x * Math.Sin(0.785398) + y * Math.Cos(0.785398)) / Math.Sqrt(2);
+                }
+                else
+                {
+                    x1 = (x * Math.Cos(2.356194) - y * Math.Sin(2.356194)) / Math.Sqrt(2) + 1;
+                    y1 = (x * Math.Sin(2.356194) + y * Math.Cos(2.356194)) / Math.Sqrt(2);
+                }
+                pixels.SetPixel(x1, y1);
+                x = x1;
+                y = y1;
+            }
+        }
 	}
 }
